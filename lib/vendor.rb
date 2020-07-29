f051136d8d2443acd957c556d32492dafcf5a9e0
Leah Riffell <leah.riffell@gmail.com>
2020-07-29 09:31:05 -0600

    Add method for adding stock to a Vendor and checking inventory

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,7 +1,24 @@
 class Vendor 
-  attr_reader :name 
-  
+  attr_reader :name, :inventory
+
   def initialize(name)
     @name = name 
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory[item] == nil 
+      0 
+    else 
+      @inventory[item] 
+    end
+  end 
+
+  def stock(item, quantity)
+    if @inventory[item] == nil
+      @inventory[item] = 0 + quantity
+    else 
+      @inventory[item] += quantity
+    end
   end
 end
