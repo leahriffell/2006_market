53a3840f5d229842ce5b989280d98735a8ab68dd
Leah Riffell <leah.riffell@gmail.com>
2020-07-29 09:39:39 -0600

    Add test for adding vendors to a Market

--- test/market_test.rb
+++ test/market_test.rb
@@ -20,4 +20,17 @@ class MarketTest < MiniTest::Test
     assert_instance_of Market, @market
     assert_equal "South Pearl Street Farmers Market", @market.name
   end
+
+  def test_it_can_add_vendors 
+    market = Market.new("South Pearl Street Farmers Market")  
+    assert_equal [], market.vendors 
+
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack") 
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal [vendor1, vendor2, vendor3], market.vendors
+  end
 end 
