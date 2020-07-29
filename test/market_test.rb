61c9c876a275e5c67bcc01b1b9073fe237caa138
Leah Riffell <leah.riffell@gmail.com>
2020-07-29 09:42:17 -0600

    Add test for returning list of Market's vendor names

--- test/market_test.rb
+++ test/market_test.rb
@@ -14,6 +14,10 @@ class MarketTest < MiniTest::Test
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
   end 
 
   def test_it_exists_and_has_readable_attributes 
@@ -32,5 +36,9 @@ class MarketTest < MiniTest::Test
     market.add_vendor(vendor2)
     market.add_vendor(vendor3)
     assert_equal [vendor1, vendor2, vendor3], market.vendors
+  end
+
+  def test_it_can_return_vendor_names 
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
   end
 end 
