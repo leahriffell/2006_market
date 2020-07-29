7979c6a4be72a4d8c4371de590947ca3e62f945f
Leah Riffell <leah.riffell@gmail.com>
2020-07-29 09:18:12 -0600

    Add tests for checking a Vendor's inventory and adding stock

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -17,4 +17,18 @@ class VendorTest < MiniTest::Test
     assert_instance_of Vendor, @vendor 
     assert_equal "Rocky Mountain Fresh", @vendor.name
   end
+
+  def test_it_can_add_inventory_and_check_stock 
+    assert_equal ({}), @vendor.inventory 
+    assert_equal 0, @vendor.check_stock(@item1)
+    
+    @vendor.stock(@item1, 30)
+    assert_equal ({@item1 => 30}), @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+    assert_equal ({@item1 => 55, @item2 => 12}), @vendor.inventory 
+  end
 end 
